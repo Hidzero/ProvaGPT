ed9db166c6d41044ae63439bd6a50a6000ea8098
Hidzero <68088463+Hidzero@users.noreply.github.com>
2023-04-04 19:31:33 -0300

    ajustes finais

--- Prova Chat GPT/api.js
+++ Prova Chat GPT/api.js
@@ -1,13 +1,13 @@
 // const pergunta pega pelo id atraves do DOM
-const pergunta = document.getElementById('pergunta');
+const pergunta = document.getElementById("pergunta");
 // const resultado pega pelo id atraves do DOM
-const resultado = document.getElementById('resultado');
+const resultado = document.getElementById("resultado");
 // Mantem a chave da API guardada
-const OPEN_API_KEY = "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"
+const OPEN_API_KEY = "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"
 
 // Se a pergunta tiver valor e o "Enter" for pressionado ele executa a função 
-pergunta.addEventListener('keypress', (e)=>{
-    if(pergunta.value && e.key === 'Enter')
+pergunta.addEventListener("keypress", (e)=>{
+    if(pergunta.value && e.key === "Enter")
     enviarPergunta();
 });
 
@@ -16,14 +16,14 @@ function enviarPergunta() {
     var sQuestion = pergunta.value;
 
     fetch("https://api.openai.com/v1/completions", {
-        method: 'POST',
+        method: "POST",
         headers:{
-            Accept: 'application/json',
-            'Content-Type': 'application/json',
-            Authorization: 'Bearer ' + OPEN_API_KEY,
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            Authorization: "Bearer " + OPEN_API_KEY,
         },
-            body: JSON.stringify({
-            model: 'text-davinci-003',
+        body: JSON.stringify({
+            model: "text-davinci-003",
             prompt: sQuestion,
             max_tokens: 2048, //Tamanho da Resposta
             temperature: 0.5, //Criatividade da Resposta
@@ -32,20 +32,20 @@ function enviarPergunta() {
         .then((response) => response.json())
         // Tratativa de erro caso de problema na api
         .then((json) => {
-            if(resultado.value) resultado.value += '\n'
+            if(resultado.value) resultado.value += "\n";
 
-            if(json.error?.message){
-                resultado.value += `Error: ${json.error.mesage}`
-            } else if(json.choices?.[0].text){
+            if(json.error?.message) {
+                resultado.value += `Error: ${json.error.mesage}`;
+            } else if(json.choices?.[0].text) {
                 var text = json.choices[0].text || 'Não tenho uma resposta';
-                resultado.value += 'Chat ' + text;
+                resultado.value += 'Chat:' + text + "\n\n";
             }
             resultado.scrollTop = resultado.scrollHeight;
         })
-        .catch((error) => console.error('Error:', error))
+        .catch((error) => console.error("Error:", error))
         .finally(() => {
-            pergunta.value = '';
-            pergunta.disabled = true;
+            pergunta.value = "";
+            pergunta.disabled = false;
             pergunta.focus();
         });
 
